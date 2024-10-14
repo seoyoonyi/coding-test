d057562f3271f5fd7fa95becf21d80e1747c0d73
seoyoon yi <89791868+seoyoonyi@users.noreply.github.com>
2024-10-14 10:52:34 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 29.61 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -0,0 +1,10 @@
+function solution(number) {
+    let answer = 0;
+    
+    for (let i = 0; i < number.length; i++) {
+        answer += parseInt(number[i], 10);
+    }
+    
+    
+    return answer%9;
+}
