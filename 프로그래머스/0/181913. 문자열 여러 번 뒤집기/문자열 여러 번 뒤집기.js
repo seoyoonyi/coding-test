b8005678ca46b1dbd89b5119d97e0e8b4587a8a8
seoyoon yi <89791868+seoyoonyi@users.noreply.github.com>
2024-10-17 10:41:11 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 19.88 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,11 +1,4 @@
 function solution(my_string, queries) {
-    let answer = '';
-    let strArray = my_string.split('');
-    
-    queries.forEach(([s,e]) => {
-        const reversedPart = strArray.slice(s, e + 1).reverse()
-        strArray.splice(s, e-s+1, ...reversedPart)
-    })
-    
-    return answer=strArray.join('');
+    var answer = '';
+    return answer;
 }
