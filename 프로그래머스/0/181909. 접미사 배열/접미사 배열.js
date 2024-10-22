cb71e884ea276298c55b1299943e832348f5bcec
seoyoon yi <89791868+seoyoonyi@users.noreply.github.com>
2024-10-22 10:45:01 +0900

    [level 0] Title: 접미사 배열, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let answer = [];
+    
+    for(let i = 0; i < my_string.length; i++) {
+        answer.push(my_string.slice(i))
+    }
+    
+    answer.sort();
+    
+    return answer;
+}
